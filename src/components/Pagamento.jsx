0d485a1feef066af574912c9a56706a692086105
Paulo <prs341450@gmail.com>
2025-12-16 15:57:24 -0300

    criando tela do zero, tela do pagamento em cartão 100%

--- src/components/Pagamento.jsx
+++ src/components/Pagamento.jsx
@@ -11,15 +11,24 @@ export default function Pagamento() {
 
             <div className="dados-hpd">
                 <h2 id="dados-pgt">1. Dados do Hóspede Principal</h2>
-
-                <label>Nome</label>
-                <input className="input-pgt" type="text" placeholder="Nome do Titular" />
-
-                <label>Sobrenome</label>
-                <input className="input-pgt" type="text" placeholder="Sobrenome do Titular" />
-
-                <h2 id="forma-pgt">2. Forma de pagamento</h2>
-                <h3 id="text-pgt">Escolha a forma de pagamento:</h3>
+                    <div className="class-name">
+                        <label id="Nome-pgt">Nome</label>
+                    </div>
+                    <div className="class-card">
+                        <input className="forma-cartao-btn" type="text" placeholder="Nome do Titular" />
+                    </div>
+                    <div className="class-sobrenome">
+                        <label id="sobrenome">Sobrenome</label>
+                    </div>
+                    <div className="class-card">
+                        <input className="forma-cartao-btn" type="text" placeholder="Sobrenome do Titular" />
+                    </div>
+                    <div className="class-forma">
+                        <h2 id="forma-pgt">2. Forma de pagamento</h2>
+                    </div>
+                    <div className="class-text">
+                        <h3 id="text-pgt">Escolha a forma de pagamento:</h3>
+                    </div>
 
                 <div className="btn-div">
                     <button
@@ -54,18 +63,34 @@ export default function Pagamento() {
 
             {forma === "cartao" && (
                 <div className="campos-cartao">
-                    <label className="details-card">Detalhes do Cartão</label>
+                    <div className="details">
+                        <label className="details-card">Detalhes do Cartão</label>
+                    </div>
                     <div className="number-card">
                         <label className="number-text">Número do Cartão</label>
-                    <input className="forma-cartao-btn" type="text" placeholder="0000 0000 0000 0000" />
+                        <input className="forma-cartao-btn" type="text" placeholder="0000 0000 0000 0000" />
                     </div>
                     <div className="nome-card">
                         <label className="form-name-text">Nome do Dono do Cartão</label>
-                    <input className="forma-cartao-btn" type="text" placeholder="Conforme impresso no cartão" />
+                        <input className="forma-cartao-btn" type="text" placeholder="Conforme impresso no cartão" />
                     </div>
                     <div>
-                        <div className="name-endereco"><label className="endereco-cob">Endereço de Cobrança</label> <label className="cvv-text">CVV</label></div>
-                        <input className="btn-date" type="text" placeholder="MM/AA" /> <input className="btn-cvv" type="password" placeholder="123" />
+                        <div className="name-cartao"><label className="endereco-cob">Data de Validade (MM/AA)</label> <label className="cvv-text">CVV</label></div>
+                        <input className="btn-date" type="date" placeholder="MM/AA" /> <input className="btn-cvv" type="password" placeholder="123" />
+                    </div>
+                    <div className="end-cob">
+                        <label className="endereco-cob">Endereço de Cobrança</label>
+                    </div>
+                    <div className="name-cartao">
+                        <label className="endereco-cob">Endereço Completo</label>
+                        <input className="forma-cartao-btn" type="text" placeholder="Rua, Número, Bairro, Cidade - UF" />
+                    </div>
+                    <div className="confirmacao">
+                        <label className="confirmacao-text">3. Confirmação</label>
+                        <div className="check-box">
+                            <input className="check-btn" type="checkbox" />
+                            <label className="termos">Aceito os <a id="termos" href="#">termos e condições</a> e as políticas de cancelamento e regra do imovel</label>
+                        </div>
                     </div>
                 </div>
             )}
