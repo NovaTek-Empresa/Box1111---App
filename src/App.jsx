cd55fd236551358ec61d1d20d29d1d20de51e8a1
Caio Felipe <eu.caiofelipeof@gmail.com>
2025-12-16 11:05:26 -0300

    arrumando erro

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,7 @@ import SellerView from './components/SellerView'
 import CompanyInfoView from './components/CompanyInfoView'
 import AllPropertiesView from './components/AllPropertiesView'
 import FavoritesView from './components/FavoritesView'
-<<<<<<< HEAD
-import ConfirmacaoDeReserva from './components/ConfirmacaoDeReserva'
-=======
-import Pagamento from './components/Pagamento'
->>>>>>> 18b1f431dfd801f2d066cb6409a95c859ff15113
+
 
 // App principal: converte a lógica do HTML original para React moderno
 // Comentários e explicações em PT-BR em cada função/parte importante
